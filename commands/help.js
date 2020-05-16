c6b8748bc7e51229ce5fe5da22f10d28c4325423
NessMC <contact@nessmc.fr>
2020-05-16 12:30:24 +0200

    🔨 Help command arguments support added.

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,8 @@ module.exports = {
     name: 'help',
     desc: 'Get Objective CLI help.',
     args: '<command>',
-    exec: function () {
+    exec: function (args, options) {
+        const command_search = options[0]
         CLIUI.div({
             text: CHALK.hex('#e32f12')(BOXEN(FIGLET.textSync('OBJECTIVE', 'Rectangles'), {padding: {bottom: 1, top: 1, right: 5, left: 5}, align: 'center'})),
             width: 70,
@@ -28,41 +29,61 @@ module.exports = {
             width: 70
         })
         CLIUI.div({text: ''})
+        CLIUI.div({
+            text: 'Commands:',
+            padding: [0, 0, 0, 5],
+            width: 20
+        },
+        {
+            text: 'Arguments:',
+            width: 15
+        },
+        {
+            text: 'Description:',
+            width: 35
+        })
+        CLIUI.div({text: ''})
         new Promise ((resolve, reject) => {
-            CLIUI.div({
-                text: 'Commands:',
-                padding: [0, 0, 0, 5],
-                width: 20
-            },
-            {
-                text: 'Arguments:',
-                width: 15
-            },
-            {
-                text: 'Description:',
-                width: 35
-            })
-            CLIUI.div({text: ''})
             FS.readdir(PATH.resolve(PATH.join(__dirname, '..', 'commands')), (error, content) => {
                 if (error) throw error
                 let counter = 0
                 for (const command of content) {
                     const app = require(PATH.resolve(PATH.join(__dirname, '..', 'commands', command)))
-                    CLIUI.div({
+                    if (command_search) {
+                        if (app.name === command_search.replace(/-/g, '')) {
+                            CLIUI.div({
+                                text: '• ' + CHALK.hex('#dddddd')(app.name),
+                                width: 20,
+                                align: 'left',
+                                padding: [0, 0, 0, 5]
+                           },
+                           {
+                                text: CHALK.greenBright(app.args),
+                                width: 15
+                           },
+                           {
+                                text: CHALK.hex('#b5b5b5')(app.desc),
+                                width: 35,
+                                align: 'left'
+                           })
+                        }
+                    } else {
+                        CLIUI.div({
                             text: '• ' + CHALK.hex('#dddddd')(app.name),
                             width: 20,
                             align: 'left',
                             padding: [0, 0, 0, 5]
-                    },
-                    {
+                       },
+                       {
                             text: CHALK.greenBright(app.args),
                             width: 15
-                    },
-                    {
+                       },
+                       {
                             text: CHALK.hex('#b5b5b5')(app.desc),
                             width: 35,
                             align: 'left'
-                    })
+                       })
+                    }
                     ++counter
                     if (counter === content.length) resolve()
                 }
@@ -71,5 +92,6 @@ module.exports = {
             CLIUI.div({text: ''})
             console.log(CLIUI.toString())
         })
+        
     }
 }
